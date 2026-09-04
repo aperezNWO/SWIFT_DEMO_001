6dbc33a2de7ef8dcb5237b330da24165804a206d
Pablo Alejandro Perez Acosta <57961103+aperezNWO@users.noreply.github.com>
2026-09-04 10:50:09 +0000

    codespaces update (ubuntu)

--- Sources/SWITF_DEMO_001/configure.swift
+++ Sources/SWITF_DEMO_001/configure.swift
@@ -1,8 +1,17 @@
 import Vapor
 
+// configures your application
 public func configure(_ app: Application) async throws {
-    // uncomment to serve files from /Public folder
-    // app.middleware.use(FileMiddleware(publicDirectory: app.directory.publicDirectory))
+    // Configure CORS
+    let corsConfiguration = CORSMiddleware.Configuration(
+        allowedOrigin: .custom("https://apereznwo.github.io"),
+        allowedMethods: [.GET, .POST, .PUT, .OPTIONS, .DELETE, .PATCH],
+        allowedHeaders: [.accept, .authorization, .contentType, .origin, .xRequestedWith]
+    )
+    let corsMiddleware = CORSMiddleware(configuration: corsConfiguration)
+    
+    // Register CORS middleware at the very beginning of the middleware stack
+    app.middleware.use(corsMiddleware, at: .beginning)
 
     // register routes
     try routes(app)
